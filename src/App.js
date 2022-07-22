0c6afbdb6439c622b638dbd2fed259a976d5b3e4
Cheaguz <aguzf83@gmail.com>
2022-07-22 00:04:23 -0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import { Header } from './components/Header/Header';
+ 
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Hello</h1>
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Header.styles.css';
+
+export const Header = () => (
+    <header className='header'>
+        <img src='assets/images/logo.png' alt='logo' />
+    </header>
+  );
